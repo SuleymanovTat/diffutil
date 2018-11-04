29eb8bf949b90b490b7d28e895a31079e29747b2
suleymanovtat <suleymanovtat@gmail.com>
2018-11-04 22:45:31 +0300

    добавил ресурсы

--- app/src/main/java/ru/suleymanovtat/diffutil/MainActivity.java
+++ app/src/main/java/ru/suleymanovtat/diffutil/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mRecyclerViewAdapter = new EmployeeRecyclerViewAdapter(
                 DummyEmployeeDataUtils.getEmployeeListSortedByRole());
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
